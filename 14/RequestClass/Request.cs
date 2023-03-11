d5c2046532279e6b3c4cc987f91b48d0db9dcd33
mmusa1@smail.ucas.edu.ps <mmusa1@smail.ucas.edu.ps>
2023-03-11 08:43:21 +0200

    Add project files.

---
+++ 14/RequestClass/Request.cs
@@ -0,0 +1,25 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestClass
+{
+    class Request
+    {
+        public static void Main(string[] args)
+        {
+            string id;
+            string url;
+            string headers;
+            Console.WriteLine("Enter Your Id");
+
+            id = Console.ReadLine();
+            Console.WriteLine("Enter Your Url ");
+            url = Console.ReadLine();
+            
+        }
+    }
+}
